a17e4bb9d666e7c7d7360b2c644380fcf872aaba
Habib Sheikh <habibsheikh.ai.groups@gmail.com>
2025-08-01 23:33:21 +0500

    Testing.py

--- Model/Testing.py
+++ Model/Testing.py
@@ -1,1 +1,16 @@
-import this and that
+import numpy as np
+import pandas as pd
+
+import torch, torchvision
+from torhc import nn, optim
+from torchmetrics.classification import Accuracy
+from torch import functional as F
+
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+from Training import Neural_Network
+
+class Testing(Neural_Network):
+    def __init__(self,):
+      pass
