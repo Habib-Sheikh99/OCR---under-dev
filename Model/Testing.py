3e3cdb3660aa51ce667f3c1dcba9f9ff2cb86e03
Habib Sheikh <habibsheikh.ai.groups@gmail.com>
2025-08-06 23:51:37 +0500

    Update Testing.py

--- Model/Testing.py
+++ Model/Testing.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 
 import torch, torchvision
-from torhc import nn, optim
+from torch import nn
+from torch.optim import Adam
 from torchmetrics.classification import Accuracy
 from torch import functional as F
 
